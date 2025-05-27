1b0d128d6b8726f72ad5a6373fc833da408438d0
Aboubacar Bagayoko <aboubacarbagayoko2000@gmail.com>
2025-05-27 01:09:54 +0000

    convertion du projet en laravel v1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,39 +1,140 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="fr">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="UTF-8">
+    <title>Authentification</title>
+    <!-- Bootstrap -->
+    <link href="{{ asset('gentelella/assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="{{ asset('gentelella/assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="{{ asset('gentelella/assets/vendors/nprogress/nprogress.css') }}" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="{{ asset('gentelella/assets/vendors/animate.css/animate.min.css') }}" rel="stylesheet">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Custom TheStyle -->
+    <link href="{{ asset('gentelella/assets/build/css/custom.min.css') }}" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href=" {{ asset('gentelella/assets/css/style.css') }}">
+    {{--
+    <link rel="stylesheet" type="text/css" href="{{ asset('gentelella/assets/build/css/style.css') }}"> --}}
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="icon" type="image" href="{{ asset('gentelella/assets/images/favicon.png') }}">
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-        @include('layouts.navigation')
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+            <div class="col-md-3 left_col">
+                <div class="left_col scroll-view">
+                    <div class="navbar nav_title" style="border: 0;">
+                        <a href="index.php?action=dashboard" class="site_title"><i class="glyphicon glyphicon-leaf"></i>
+                            <span>IDocsMali
+                            </span></a>
+                    </div>
 
-        <!-- Page Heading -->
-        @isset($header)
-        <header class="bg-white dark:bg-gray-800 shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
+                    <div class="clearfix"></div>
+
+                    <!-- je charge le menu profile  -->
+                    {{-- @yield('layouts.profileMenu') --}}
+                    <!-- /menu profile  -->
+
+                    <br />
+
+                    <!-- je charge le sidebar menu -->
+                    @include('layouts.sidebar')
+                    <!-- /sidebar menu -->
+
+                    <!-- je charge le menu footer buttons -->
+                    {{-- @include('menuFooter') --}}
+                    <!-- /menu footer buttons -->
+                </div>
             </div>
-        </header>
-        @endisset
 
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
+            <!-- je charge la bar de navigation du haut-->
+
+            <!-- /top navigation -->
+
+            @yield('content')
+            <!-- /page content -->
+
+            <!-- footer content -->
+            {{-- @include('layouts.footer') --}}
+            <!-- /footer content -->
+        </div>
     </div>
+
+
+    <!-- Je charge les fichier du template -->
+    <!-- jQuery -->
+    <script src="{{ asset('gentelella/assets/vendors/jquery/dist/jquery.min.js')}}"></script>
+    <!-- Bootstrap -->
+    <script src="{{ asset('gentelella/assets/vendors/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- FastClick -->
+    <script src="{{ asset('gentelella/assets/vendors/fastclick/lib/fastclick.js')}}"></script>
+    <!-- NProgress -->
+    <script src="{{ asset('gentelella/assets/vendors/nprogress/nprogress.js')}}"></script>
+    <!-- Chart.js -->
+    <script src="{{ asset('gentelella/assets/vendors/Chart.js/dist/Chart.min.js')}}"></script>
+    <!-- gauge.js -->
+    <script src="{{ asset('gentelella/assets/vendors/gauge.js/dist/gauge.min.js')}}"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="{{ asset('gentelella/assets/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js')}}"></script>
+    <!-- iCheck -->
+    <script src="{{ asset('gentelella/assets/vendors/iCheck/icheck.min.js')}}"></script>
+    <!-- Skycons -->
+    <script src="{{ asset('gentelella/assets/vendors/skycons/skycons.js')}}"></script>
+    <!-- Flot -->
+    <script src="{{ asset('gentelella/assets/vendors/Flot/jquery.flot.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/Flot/jquery.flot.pie.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/Flot/jquery.flot.time.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/Flot/jquery.flot.stack.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/Flot/jquery.flot.resize.js')}}"></script>
+    <!-- Flot plugins -->
+    <script src="{{ asset('gentelella/assets/vendors/flot.orderbars/js/jquery.flot.orderBars.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/flot-spline/js/jquery.flot.spline.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/flot.curvedlines/curvedLines.js')}}"></script>
+    <!-- DateJS -->
+    <script src="{{ asset('gentelella/assets/vendors/DateJS/build/date.js')}}"></script>
+    <!-- JQVMap -->
+    <script src="{{ asset('gentelella/assets/vendors/jqvmap/dist/jquery.vmap.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js')}}"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="{{ asset('gentelella/assets/vendors/moment/min/moment.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="{{ asset('gentelella/assets/build/js/custom.min.js')}}"></script>
+
+    <!-- morris.js -->
+    <script src="{{ asset('gentelella/assets/vendors/raphael/raphael.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/morris.js/morris.min.js')}}"></script>
+
+    <!-- Datatables -->
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js')}}">
+    </script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-buttons/js/buttons.flash.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js')}}">
+    </script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js')}}">
+    </script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-responsive/js/dataTables.responsive.min.js')}}">
+    </script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js')}}">
+    </script>
+    <script src="{{ asset('gentelella/assets/vendors/datatables.net-scroller/js/dataTables.scroller.min.js')}}">
+    </script>
+    <script src="{{ asset('gentelella/assets/vendors/jszip/dist/jszip.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/pdfmake/build/pdfmake.min.js')}}"></script>
+    <script src="{{ asset('gentelella/assets/vendors/pdfmake/build/vfs_fonts.js')}}"></script>
 </body>
 
 </html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,59 @@
+<!-- Sidebar -->
+<div class="col-md-3 left_col">
+    <div class="left_col scroll-view">
+        <div class="navbar nav_title" style="border: 0;">
+            <a href="{{ url('/') }}" class="site_title"><i class="fa fa-paw"></i> <span>iDocs Mali</span></a>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <!-- Menu profil -->
+        <div class="profile clearfix">
+            <div class="profile_pic">
+                <img src="{{ asset('assets/images/user.png') }}" alt="..." class="img-circle profile_img">
+            </div>
+            <div class="profile_info">
+                <span>Bienvenue,</span>
+                <h2>{{ session('user')['nom'] ?? 'Utilisateur' }}</h2>
+            </div>
+        </div>
+
+        <br />
+
+        <!-- Menu dynamique -->
+        <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+            <div class="menu_section">
+                <h3>Menu</h3>
+                <ul class="nav side-menu">
+                    {{-- @if (session('user')['role'] === 'citoyen') --}}
+                    <li><a href="{{ url('index.php?action=new_demande') }}"><i class="fa fa-plus"></i> Nouvelle
+                            demande</a></li>
+                    <li><a href="#"><i class="fa fa-folder"></i> Mes demandes</a></li>
+                    <li><a href="#"><i class="fa fa-users"></i> Ma famille</a></li>
+                    {{-- @elseif (session('user')['role'] === 'admin') --}}
+                    <li><a href="#"><i class="fa fa-dashboard"></i> Tableau de bord</a></li>
+                    <li><a href="#"><i class="fa fa-user"></i> Gestion des utilisateurs</a></li>
+                    <li><a href="#"><i class="fa fa-folder-open"></i> Toutes les demandes</a></li>
+                    <li><a href="#"><i class="fa fa-cog"></i> Paramètres</a></li>
+                    {{-- @endif --}}
+                </ul>
+            </div>
+        </div>
+
+        <!-- Footer sidebar -->
+        <div class="sidebar-footer hidden-small">
+            <a data-toggle="tooltip" data-placement="top" title="Paramètres">
+                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="Verrouiller">
+                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="Déconnexion" href="{{ url('/logout') }}">
+                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+            </a>
+        </div>
+    </div>
+</div>
