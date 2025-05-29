48b442fa47da6fa92d47be3c4368bb71b74bdba1
cebdgit <diarracheickelboukader@gmail.com>
2025-05-29 18:04:37 +0000

    first commit cheick

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,10 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'nom',
+        'prenom',
+        'adresse',
+        'telephone',
         'email',
         'password',
     ];
