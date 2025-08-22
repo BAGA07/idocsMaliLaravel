e123009c2664eaf34bc5e1351fb054d0b546459c
Aboubacar Bagayoko <aboubacarbagayoko2000@gmail.com>
2025-08-22 20:26:03 +0000

    version badara

--- resources/views/hopital/naissances/show.blade.php
+++ resources/views/hopital/naissances/show.blade.php
@@ -185,7 +185,7 @@
             <label for="email" class="block mb-2 text-sm font-medium text-blue-700">Email</label>
             <input type="email" name="email" id="email"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
-              placeholder="renseigner l'email du declarant">
+              placeholder="{{ $declaration->declarant->email ?? 'Non renseigné' }}">
           </div>
           <div>
             <label for="telephone" class="block mb-2 text-sm font-medium text-blue-700">Téléphone</label>
