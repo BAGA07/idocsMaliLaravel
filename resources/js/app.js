68e42d99e44cda078e3654bea8bde5c01f83fef7
cebdgit <diarracheickelboukader@gmail.com>
2025-09-04 12:05:59 +0000

    correction css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,15 @@
+// // resources/js/app.js
+
+// import './bootstrap'; // Gardez cette ligne si vous l'utilisez pour d'autres configurations Laravel
+
+// // Importation CORRECTE de Flowbite. C'est tout.
+// import 'flowbite';
 // resources/js/app.js
 
-import './bootstrap'; // Gardez cette ligne si vous l'utilisez pour d'autres configurations Laravel
+import './bootstrap'; // garder si utilisé pour axios/echo/etc
 
-// Importation CORRECTE de Flowbite. C'est tout.
+// Importation de Font Awesome (icônes)
+import '@fortawesome/fontawesome-free/css/all.css';
+
+// Importation de Flowbite (composants)
 import 'flowbite';
